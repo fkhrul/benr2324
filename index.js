d0a7b20549cc80c2d640bb6cfd1e4537e58a5858
fkhrul <b022210160@student.utem.edu.my>
2024-03-25 21:36:21 +0800

    fkhrul

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 2000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('Hello bai')
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
